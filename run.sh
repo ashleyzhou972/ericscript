b354866e3f0bc244dbc534ea129969ebaae50af6
EC2 Default User <ec2-user@ip-10-42-15-118.us-west-2.compute.internal>
2021-01-21 23:07:44 +0000

    initial add

---
+++ run.sh
@@ -0,0 +1,2 @@
+#! /usr/bin/bash
+./ericscript.pl --simulator --db ../ericscript_db_homosapiens_ensembl73/ -o ~/data/bio-5943-mojo-synthetic/fixed_no_repeat_6_1/ --ngenefusion 6 --nsims 1 -rl 75 -v --background_1 /home/ec2-user/data/bio-5943-mojo-synthetic/background_data/20-A73997_RSQ1/20-A73997_RSQ1_1.fastq --background_2 /home/ec2-user/data/bio-5943-mojo-synthetic/background_data/20-A73997_RSQ1/20-A73997_RSQ1_3.fastq --nreads_background 200000 --min_cov 1000 --max_cov 1000
---
+++ run_fixed.sh
@@ -0,0 +1,2 @@
+#! /usr/bin/bash
+./ericscript_tempus_fixed.pl --simulator --db ../ericscript_db_homosapiens_ensembl73/ -o $1  --nsims 5 -rl 75 -v --background_1 /home/ec2-user/data/bio-5943-mojo-synthetic/background_data/20-A73997_RSQ1/20-A73997_RSQ1_1.fastq --background_2 /home/ec2-user/data/bio-5943-mojo-synthetic/background_data/20-A73997_RSQ1/20-A73997_RSQ1_3.fastq --nreads_background 200000 --min_cov 500 --max_cov 500 -ngenefusion 2
